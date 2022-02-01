31319cdfa48dbdcfa88be9d667f3644b15dd896e
NumanKarsli <numankarsli25@gmail.com>
2022-02-01 20:27:10 +0300

    bazı şeyler değiştirildi

--- 08_counting_syllables.py
+++ 08_counting_syllables.py
@@ -7,7 +7,6 @@ def count(string):
             
     return sayac+1
 
-
 # naive solution
 def count(word):
     syllables = 1
@@ -16,10 +15,5 @@ def count(word):
             syllables = syllables + 1
     return syllables
 
-# using the count method
-def count(word):
-    return word.count("-") + 1
 
-# using split
-def count(word):
-    return len(word.split("-"))
+# Numan burayı değiştirdi
